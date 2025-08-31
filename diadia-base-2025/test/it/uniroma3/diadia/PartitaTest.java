0241858980beec7c3a13e5e666a95631bd23ece7
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-08-31 19:07:13 +0200

    aggiunti package

--- diadia-base-2025/test/it/uniroma3/diadia/PartitaTest.java
+++ diadia-base-2025/test/it/uniroma3/diadia/PartitaTest.java
@@ -7,6 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.uniroma3.diadia.ambienti.Stanza;
+
 class PartitaTest {
 
 	private Partita partita;
