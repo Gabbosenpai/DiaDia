8bbfb722ce058d0c66eb17ff13c55021c0a2fb68
Gabbosenpai <gabrielemoglie493.gm@gmail.com>
2025-09-11 01:23:48 +0200

    Abstract comando

---
+++ diadia-base-2025/diadia/src/it/uniroma3/diadia/comandi/AbstractComando.java
@@ -0,0 +1,37 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.Partita;
+
+public abstract class AbstractComando {
+	
+	private String parametro;
+	private IO io;
+	private String nome;
+	
+	public abstract void esegui(Partita partita);
+	
+	public void setParametro(String parametro) {
+		this.parametro = parametro;
+	}
+	
+	public String getParametro() {
+		return this.parametro;
+	}
+	
+	public IO getIO() {
+		return this.io;
+	}
+	
+	public void setIO(IO io) {
+		this.io = io;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
